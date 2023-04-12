2a35d4e4ac22cb08222a4c642729201fc75f28aa
tangshixiang <tangshixiang2016@gmail.com>
2023-04-12 16:48:13 +0800

    UPDATE

---
+++ PATH/core/testers/__init__.py
@@ -0,0 +1,6 @@
+from .reid_tester import ReIDTester
+from .fr_tester import FRTester
+from ..solvers.solver_multitask_dev import TesterMultiTaskDev, PLMultiTaskDev
+
+def tester_entry(C_train, C_test):
+    return globals()[C_test.config['common']['tester']['type']](C_train, C_test)
